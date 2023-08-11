0bd7b8138ccbbcc43be4763099fb002156e9dfc1
samt11345 <Samuel.tahjai.williams@gmail.com>
2023-08-10 21:04:33 -0400

    added the test because i forgot!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const path = require('path');
+// const path = require('path');
 class Svg {
     constructor() {
         this.textElement = ''
